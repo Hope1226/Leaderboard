13d4675607735bbcfe8a84da3c81902765519d78
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-11-10 03:14:35 +0500

    Create recent scores section via module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.scss';
+import renderScoreBoard from './app/recentScores.js';
+
+const appContainer = document.querySelector('.app-container');
+appContainer.appendChild(renderScoreBoard());
