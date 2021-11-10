40722849bb9783b9e2c5f79465ed3baccd60617a
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-11-10 19:56:42 +0500

    Add missing packages and update Readme file

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+
+    static: './dist',
+
+  },
   plugins: [
 
     new HtmlWebpackPlugin({
